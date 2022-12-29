e15756f81babdb212594f35cd253420a31bd7d39
Leticia BuCor <cursocompartido@gmail.com>
2022-12-29 15:24:24 -0600

    Anexo dos añadidos

---
+++ Linq_05ArrayList/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_05ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tips para el uso de ArrayList con Linq por medio de OfType<>
+            //seleccionamos objetos de un tipo en particular que estan en un ArrayList
+
+            ArrayList lista = new ArrayList();
+            lista.AddRange(new object[] { "hola", 5, 6.7, false, 4, 2, "saludos", 3.5, 3 });
+
+            //Obtenemos solo los enteros
+            var enteros = lista.OfType<int>();
+            Console.WriteLine("-----Muestra solo los enteros del arreglo-");
+            foreach (int item in enteros)
+                Console.WriteLine(item);
+
+            Console.WriteLine("-----Muestra solo los strings del arreglo-");
+
+            var palabras = lista.OfType<string>();
+            foreach(string item in palabras)
+                Console.WriteLine(item);
+
+            Console.WriteLine("------");
+
+            //Se crea un arrayList
+           ArrayList aListEstudiantes = new ArrayList()
+            {
+                new CEstudiante("ana","01","Mercadotecnia",1.0),
+                new CEstudiante("luis","02","Programación",9.0),
+                new CEstudiante("Susana","03","Inglés",9.5),
+                new CEstudiante("Pablo","04","Programación",10.0),
+                new CEstudiante("Alberto","05","Mercadotecnia",4)
+            };
+
+            //Se transforma el ArrayList a un tipo que implemente a IEnumerable<T> para poder 
+            //ser usaco con Linq
+            var IEnumAListEstudiantes = aListEstudiantes.OfType<CEstudiante>();
+
+            //Ahora si  se puede usar linq para encontrar a los abrobados
+            var aprobados = from item in IEnumAListEstudiantes
+                            where item.Promedio >= 8
+                            select item;
+            Console.WriteLine("----Aprobados");
+
+            foreach(CEstudiante item in aprobados)
+                Console.WriteLine(item);
+
+            Console.WriteLine("--Proyección se necesita de tipos anonimos");
+            List<CEstudiante> listEstudiantes = new List<CEstudiante>
+            {
+                new CEstudiante("ana","01","Mercadotecnia",1.0),
+                new CEstudiante("luis","02","Programación",9.0),
+                new CEstudiante("Susana","03","Inglés",9.5),
+                new CEstudiante("Pablo","04","Programación",10.0),
+                new CEstudiante("Alberto","05","Mercadotecnia",4)
+            };
+
+            //Aqui es donde se realiza la proyección con el tipo anonimo en el select
+            var nombrePromedio = from item in listEstudiantes
+                                 where item.Promedio>9
+                                 select new { item.Nombre, item.Promedio };
+
+            foreach(var item in nombrePromedio)
+                Console.WriteLine(item);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Linq_07Operaciones/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_07Operaciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<CEstudiante> listEstudiantes = new List<CEstudiante>
+            {
+                new CEstudiante("ana","01","Mercadotecnia",1.0),
+                new CEstudiante("luis","02","Programación",9.0),
+                new CEstudiante("Susana","03","Inglés",9.5),
+                new CEstudiante("Pablo","04","Programación",10.0),
+                new CEstudiante("Alberto","05","Mercadotecnia",4)
+            };
+
+            Console.WriteLine("---Conteo");
+            int cantidad = (from item in listEstudiantes
+                            where item.Promedio > 5
+                            select item).Count();
+
+            Console.WriteLine("La cantidad de aprobados es: {0}",cantidad);
+
+            Console.WriteLine("----Orden inverso");
+            var aprobados = from item in listEstudiantes
+                            where item.Promedio > 5
+                            select item;
+
+            foreach (CEstudiante item in aprobados)
+                Console.WriteLine(item);
+            Console.WriteLine("-----");
+            foreach (CEstudiante item in aprobados.Reverse())
+                Console.WriteLine(item);
+
+            Console.WriteLine("---Ordenamiento");
+            var ordenadosDes = from elem in listEstudiantes
+                               orderby elem.Promedio descending
+                               select elem;
+
+            foreach(CEstudiante item in ordenadosDes)
+                Console.WriteLine(item);
+
+            Console.WriteLine("---Ascendente");
+            var ordenadosAsc = from elem in listEstudiantes
+                               orderby elem.Promedio ascending
+                               select elem;
+
+            foreach (CEstudiante item in ordenadosAsc)
+                Console.WriteLine(item);
+
+            Console.WriteLine("---------\n");
+            int[] numeros = { 2, 5, 9, 1, 6, 7, 8 };
+
+             foreach(var item in numeros)
+                Console.WriteLine(item);
+
+
+            //Encontrando el maximo
+            int maximo = (from elem in numeros select elem).Max();
+            Console.WriteLine("El maximo es:{0}",maximo);
+
+            //Encontando el minimo
+            int minimo = (from elem in numeros select elem).Min();
+            Console.WriteLine("El minimo es:{0}",minimo);
+
+            //Encontrando el promedio
+            double promedio = (from elem in numeros select elem).Average();
+            Console.WriteLine("El promedio es:{0}",promedio);
+
+            //Sumatoria
+            int sumatoria = (from elem in numeros select elem).Sum();
+            Console.WriteLine("La sumatoria es:{0}",sumatoria);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
